7690f994fe654c57e7c20bbb7df70801320ce6b1
Squareben1 <bengittins@live.com>
2020-02-18 12:36:06 +0000

    refactored

--- lib/grade_report.rb
+++ lib/grade_report.rb
@@ -1,5 +1,19 @@
 class GradeReportClass
   def grade_report(string)
-    "#{string}: 1"
+    green_count = string.count("G")
+    amber_count = string.count("A")
+    red_count = string.count("R")
+    #string.split(,).length - greencount, red, amber - gives lenght of array with proepr colours removed i.e. uncounted 
+    x = []
+    if green_count > 0
+      x.push("Green: #{green_count}")
+    end
+    if amber_count > 0
+      x.push("Amber: #{amber_count}")
+    end
+    if red_count > 0
+      x.push("Red: #{red_count}")
+    end
+    return x.join("\n")
   end
 end
--- spec/grade_report_spec.rb
+++ spec/grade_report_spec.rb
@@ -6,8 +6,37 @@ describe GradeReportClass do
       expect(subject.grade_report("Green")).to eq "Green: 1"
     end
 
+    it 'returns Red: 1 when passed 1 Red' do 
+      expect(subject.grade_report("Red")).to eq "Red: 1"
+    end
+
     it 'returns Amber: 1 when passed 1 Amber' do 
       expect(subject.grade_report("Amber")).to eq "Amber: 1"
     end
+
+    it 'returns Green: 2 when passed 2 Greens' do 
+      expect(subject.grade_report("Green, Green")).to eq "Green: 2"
+    end
+
+    it 'returns Amber: 2 when passed 2 Ambers' do 
+      expect(subject.grade_report("Amber, Amber")).to eq "Amber: 2"
+    end
+
+    it 'returns Green: 1\nAmber: 1 when passed 1 green 1 Amber' do 
+      expect(subject.grade_report("Green, Amber")).to eq "Green: 1\nAmber: 1"
+    end
+
+    it 'returns Green: 1\nAmber: 2 when passed 1 green 2 Ambers' do 
+      expect(subject.grade_report("Green, Amber, Amber")).to eq "Green: 1\nAmber: 2"
+    end
+
+    it 'returns Green: 1\nAmber: 1\nRed: 1 when passed 1 green 1 Amber, 1 red' do 
+      expect(subject.grade_report("Green, Amber, Red")).to eq "Green: 1\nAmber: 1\nRed: 1"
+    end
+
+    it 'returns Green: 1\nAmber: 2 when passed 1 green 2 Ambers' do 
+      expect(subject.grade_report("Green, Amber, Green, Red, Amber, Red")).to eq "Green: 2\nAmber: 2\nRed: 2"
+    end
+
   end
 end
